78f0cb4ad8aa38ea943c254da0710c5f46c6bba8
user <user@24-003441.iteco-inno.ru>
2024-09-19 17:53:05 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import NewsItem from './NewsItem/NewsItem'
+import { useEffect } from 'react';
+
+const initNews = [
+  {
+    title: 'GFirst news',
+    username: 'Fire',
+    url: 'www.ex.com',
+    date: '11.04.24',
+    score: 40,
+    id: '89898',
+  },
+  {
+    title: 'Second news',
+    username: 'Water',
+    url: 'www.ex6.com',
+    date: '14.06.24',
+    score: 50,
+    id: '345',
+  },
+  {
+    title: 'Second news',
+    username: 'Edge',
+    url: 'www.ex2.com',
+    date: '11.03.24',
+    score: 20,
+    id: '9854',
+  }
+];
+
+const newNews = {
+  title: 'Third news',
+  username: 'Edge444',
+  url: 'www.ex2.com',
+  date: '11.03.24',
+  score: 78,
+  id: '232',
+};
 
 function App() {
+  const checkStorage = () => JSON.parse(window.localStorage.getItem('newsKey')) || initNews;
+  
+  const [news, setNews] = useState(checkStorage);
+  
+  useEffect(() => {
+    window.localStorage.setItem('newsKey', JSON.stringify(news))
+  }, [news])
+
+  const newCountHandler = () => {
+    setNews((prev) => [...prev, {...newNews, id: Date.now()}]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Колличество новостей: {news.length}</div>
+      <button onClick={newCountHandler}>Add news</button>
+      {
+        news.map(item => {
+          return <NewsItem 
+                      key={item.id}
+                      title={item.title} 
+                      url={item.url}
+                      date={item.date}
+                      username={item.username}
+                      score={item.score}
+                      />
+        })
+      }
     </div>
   );
 }
---
+++ src/NewsItem/NewsItem.js
@@ -0,0 +1,21 @@
+import style from "./NewsItem.module.css"
+
+export default function NewsItem(props) {
+    
+    return (
+        <div className={style.container}>
+            <a className={style.link} href="example.com">{props.title}</a>
+        <div className={style.info}>
+            <div className={style.userData}>
+                <span>{props.username}</span>
+                <span>{props.date}</span>
+            </div>
+
+            <div className={style.score}>
+            {props.score}
+            </div>
+        </div>
+            
+        </div>
+    )
+}
