8cce1d99c4c8ad660ad4901dad389c05f1347f05
user <user@24-003441.iteco-inno.ru>
2024-09-24 12:46:05 +0300

    Add pages

---
+++ src/Components/NewsItem/NewsItem.js
@@ -0,0 +1,34 @@
+import style from "./NewsItem.module.css";
+import { unixToDate } from "../../utils/utils";
+import { Link } from "react-router-dom";
+
+export function NewsItem(props) {
+  const { className = '', title, username, date, score } = props;
+  const scoreClassArr = [style.score];
+
+  if (score > 50) {
+    scoreClassArr.push(style.highScore);
+  } else if (score > 30) {
+    scoreClassArr.push(style.midScore);
+  } else {
+    scoreClassArr.push(style.lowScore);
+  }
+
+  return (
+    <div className={`${style.container} ${className}`}>
+        <Link className={style.link} to={`comments/${props.id}`}>{title}</Link>
+      {/* <a className={style.link} href="example.com">
+        {title}
+      </a> */}
+      <div className={style.info}>
+        <div className={style.userData}>
+          <span>{username}</span>
+          <span> | </span>
+          <span>{unixToDate(date)}</span>
+        </div>
+
+        <div className={scoreClassArr.join(" ")}>{score}</div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/NewsList/NewsList.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import { NewsItem } from "../../Components/NewsItem/NewsItem";
+import { get } from "../../api/api";
+import styles from './NewsList.module.css'
+
+function NewsList() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    getNewsList();
+  }, []);
+
+  async function getNewsList() {
+    const newsIds = await get(
+      'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&&orderBy="$priority"&limitToFirst=10'
+    );
+    const newsList = await Promise.all(
+      newsIds.map((id) =>
+        get(
+          `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+        )
+      )
+    );
+    setNews(newsList);
+  }
+
+  return (
+    <div>
+      <div>Колличество новостей: {news.length}</div>
+      {news.map((item) => {
+        return (
+          <NewsItem
+            className={styles.newsItem}
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            url={item.url}
+            date={item.time}
+            username={item.by}
+            score={item.score}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default NewsList;
